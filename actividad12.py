80c0c56d1f6d8840a4c6e55cb6c476048f4c7603
sofidep08 <sx.dpaguirre@udeo.edu.gt>
2025-08-07 23:29:25 -0600

    arreglos

--- actividad12.py
+++ actividad12.py
@@ -1,5 +1,4 @@
-diccionario={}
-lista=[]
+
 def quick_sort(lista):
     if len(lista) <=1:
         return lista
@@ -9,18 +8,18 @@ def quick_sort(lista):
     ultimo = [x for x in lista[1:] if x > pivote]
     return quick_sort(primero) + igual + quick_sort(ultimo)
 
-def busqueda(lista,buscando):
-    for elementos in lista:
-       if elementos == buscando:
-            return elementos
-       else:
-           return print("No esta el empleado")
+def buscar_repartidor(diccionario, nombre_buscado):
+    for nombre, datos in diccionario.items():
+        if nombre.lower() == nombre_buscado.lower():
+            return nombre, datos
+    return None
 
-def mostrar(lista):
-    for elementos in lista:
-        print(elementos)
+def mostrar(diccionario_ordenado):
+    print("\nRepartidores registrados")
+    for nombre, datos in diccionario_ordenado:
+        print(f"Nombre: {nombre}, Paquetes: {datos['paquetes']}, Zona: {datos['zona']}")
 
-
+diccionario={}
 cantidad= int(input("Ingrese la cantidad de personas que desea registrar: "))
 for i in range(cantidad):
     nombre=input("Ingrese el nombre del repartidor: ")
@@ -30,10 +29,6 @@ for i in range(cantidad):
         "paquetes":paquetes,
         "zona":zona
     }
-    lista.append((nombre,paquetes,zona))
-quick_sort(lista)
-mostrar(lista)
+lista_ordenada = quick_sort(list(diccionario.items()))
 
-buscar=input("Ingrese al repartidor que desea buscar: ")
-encontrado=busqueda(lista,buscar)
-print(encontrado)
+mostrar(lista_ordenada)
